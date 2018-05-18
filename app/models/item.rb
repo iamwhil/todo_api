aea1c87c18a1235e9bc70ca9c274a0a3825769e4
Whil Piavis <ncsupiratecaptain@gmail.com>
2018-05-17 20:32:53 -0600

    Initial commit.

---
+++ app/models/item.rb
@@ -0,0 +1,9 @@
+class Item < ApplicationRecord
+
+  # Associations
+  belongs_to :todo
+
+  # Validations
+  validates_presence_of :name
+  
+end
---
+++ app/models/todo.rb
@@ -0,0 +1,10 @@
+class Todo < ApplicationRecord
+
+  # Model associations
+  has_many :items, dependent: :destroy
+
+  # Validations
+
+  validates_presence_of :title, :created_by
+
+end
